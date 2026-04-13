4c86aaa6c6cf0f4cc463a9259935a2262b843899
Heidi Bucio Martinez <buciomartinezh@xavier.edu>
2026-04-13 17:40:00 -0400

    Add the main.java and update the RandomEnemy.java so no error occurs.

--- src/main/java/RandomEnemy.java
+++ src/main/java/RandomEnemy.java
@@ -1,3 +1,10 @@
 public class RandomEnemy extends Character{
     //need to update with differnt character types so enemeies are not just all the same
+    public RandomEnemy(Weapon weapons, Armor armors, String names) {
+        super((int) (Math.random() * 11) + 20,   // 20–30 health
+                (int) (Math.random() * 6) + 5,     // 5–10 mana
+                weapons, armors, names, //supers these
+                1, //1 inventory
+                (int) (Math.random() * 10) + 1); // 1-10 speed
+    }
 }
